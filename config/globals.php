5ccc0ad6e33ff07f6aa8bf9a2d1a043fb46d020b
xavierpioche <xavier.pioche@laposte.net>
2023-11-04 15:08:33 -0400

    add generic config

---
+++ config/globals.php
@@ -0,0 +1,14 @@
+<?php
+$user = "usr_asyncmeeting";
+$password = "aze123QSD!";
+$database = "asyncmeeting";
+$db_host = "localhost";
+
+$redis_db = "127.0.0.1";
+$redis_port = "6379";
+
+$language=0;
+
+const APP_URL = 'http://www.asyncmeeting.com/';
+const SENDER_EMAIL_ADDRESS = 'no-reply@email.com';
+?>
